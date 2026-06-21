902ed2e8c60c5c85e71c204f4602307930175807
Alexander Jacob <alex.r.jacob8@gmail.com>
2026-06-21 12:39:00 +1000

    do lab 2 basic arithmetic

--- lab2/basic_arithmetic/src/basic_arithmetic.s
+++ lab2/basic_arithmetic/src/basic_arithmetic.s
@@ -10,8 +10,19 @@ asm_entry                 			; This is a label, the entry to the main function.
 
 			;;;;;;;;;;;;;;;;;;;;;;;;
 	
-			; Your code goes here.			
-			
+			mov r0, #0				; set r0 to 0
+			ldr r1, [pc, #24]		; load a into r1
+
+			ldr r2, [pc #36]		; load d into r2
+			add r1, r1, r2			; r1 = (a+d)
+
+			ldr r3, [pc, #16]		; load c into r3
+
+			ldr r4, [pc #20]		; load b into r4
+			add r3, r3, r4			; r3 = (c+b)
+
+			sub r0, r1, r3			; r0 = (a+d) - (c+b)
+
 			;;;;;;;;;;;;;;;;;;;;;;;;
                                                                          
 			b   asm_entry          	; this is an instruction which sets pc back to main
