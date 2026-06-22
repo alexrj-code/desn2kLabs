82b7a93f96779d474789f6217e4cf2ba26e12d54
Alexander Jacob <alex.r.jacob8@gmail.com>
2026-06-22 11:14:13 +1000

    simplify gcd

--- lab2/gcd/src/gcd.s
+++ lab2/gcd/src/gcd.s
@@ -1,16 +1,19 @@
 			AREA gcd, CODE
 			EXPORT asm_entry  
-						
+
 asm_entry                      		
 			; Here a is 12 and b is 2, make sure
             ; you try different values when testing.
 
 			mov r0, #12		; a
 			mov r1, #2      ; b
-			
-            ; Place your code here
 
-			b   asm_entry
+asm_entry_cond_0
+			cmp r0, r1
+			beq asm_entry_end
+			subgt r0, r0, r1
+			suble r1, r1, r0
+			b 	 asm_entry_cond_0
 
-
+asm_entry_end
 			END
