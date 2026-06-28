30beab8bc2615e471b985acf096b19b623cbdbed
Alexander Jacob <alex.r.jacob8@gmail.com>
2026-06-28 11:13:53 +1000

    char_toupper

--- lab2/char_toupper/src/char_toupper.s
+++ lab2/char_toupper/src/char_toupper.s
@@ -3,9 +3,23 @@
 						
 asm_entry                      		
 					
-			; Write your code here.
+			# check if between 0x61 and 0x67 inclusive
+			# if so, minus 32_d
+			# else break
+			ldr r0, =char 			# loads address of char into r0
+			ldrb r1, [r0]			# tells it to load the actual char BYTE from address
 
-			b   asm_entry
+			cmp r1, #0x61			# cmp with 'a'
+			blt skip				# if less than 'a' its either uppercase already or invalid, we don't care
+			cmp r1, #0x67			# cmp with 'g'
+			bgt skip				# if greater than 'g' its beyond the scope, skip
+
+			# executes if a<= r1 <= g
+			sub r1, r1, #32
+			strb r1, [r0]
+
+skip
+			END
 
 			AREA character, DATA
 char		DCB "f"				; identical to DCB 0x66
