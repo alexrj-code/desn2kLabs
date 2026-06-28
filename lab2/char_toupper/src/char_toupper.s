2ef1c335f277ecd6a656b7cf9a47289a7f3f41fa
Alexander Jacob <alex.r.jacob8@gmail.com>
2026-06-28 11:42:34 +1000

    string_to_int

--- lab2/char_toupper/src/char_toupper.s
+++ lab2/char_toupper/src/char_toupper.s
@@ -3,16 +3,16 @@
 						
 asm_entry                      		
 					
-			# check if between 0x61 and 0x67 inclusive
-			# if so, minus 32_d
-			# else break
-			ldr r0, =char 			# loads address of char into r0
-			ldrb r1, [r0]			# tells it to load the actual char BYTE from address
+			; check if between 0x61 and 0x67 inclusive
+			; if so, minus 32_d
+			; else break
+			ldr r0, =char 			; loads address of char into r0
+			ldrb r1, [r0]			; tells it to load the actual char BYTE from address
 
-			cmp r1, #0x61			# cmp with 'a'
-			blt skip				# if less than 'a' its either uppercase already or invalid, we don't care
-			cmp r1, #0x67			# cmp with 'g'
-			bgt skip				# if greater than 'g' its beyond the scope, skip
+			cmp r1, #0x61			; cmp with 'a'
+			blt skip				; if less than 'a' its either uppercase already or invalid, we don't care
+			cmp r1, #0x67			; cmp with 'g'
+			bgt skip				; if greater than 'g' its beyond the scope, skip
 
 			# executes if a<= r1 <= g
 			sub r1, r1, #32
