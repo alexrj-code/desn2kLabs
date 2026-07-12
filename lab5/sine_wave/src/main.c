2ecfca6daed09a05733a7ccbccbb1e56b27e5dd8
Alexander Jacob <alex.r.jacob8@gmail.com>
2026-07-13 09:38:27 +1000

    do lab 5i

--- lab5/sine_wave/src/main.c
+++ lab5/sine_wave/src/main.c
@@ -4,15 +4,26 @@
 
 extern unsigned int sinewave_data[];
 
+// WE R TOLD DAC ON QVGA IS PHYSICALLY CONNECTED TO SPEAKER 
+
 int main(void) {
 	int i = 0;
 	
 	// Setup Pin Connect for AOUT
-	
+	PINSEL1 = PINSEL1 & ~(3 << 20);			// we want bits 20-21 to be 00 initially and everything else the same
+	PINSEL1 = PINSEL1 | (2 << 20);			// set them to 10 (DAC OUTPUT) -> see table ss
+
 	
 	while (1) {
 		//Write next value of sinewave_data[] to the DAC
+		DACR = (sinewave_data[i] << 6);		// put the data into DACR, shift so we don't touch forbidden section
+		i = i + 1;
 		
+		// won't this be way too high freq to hear???
+
+		if (i == 256) {
+            i = 0;
+        }
 	}
 	
 	return 0;
